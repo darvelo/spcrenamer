22ea1bb8ac713e33d3fcc13152d44e29ca4cd61f
David Arvelo <david@davidarvelo.com>
2014-08-15 23:37:52 -0400

    Even better parsing; validation; cleanup

--- src/Package.hpp
+++ src/Package.hpp
@@ -3,33 +3,73 @@
 
 #include <string>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 class Package {
 public:
+    /* constructors */
+    Package() : valid(false) { };
     Package(std::string fname);
+
+    /* variables */
     static std::string baseDir;
-    void outputName(std::ostream& os = std::cout) const
-        { os << baseDir << "/" << filename << std::endl; }
+    static std::string extension;
+
+    /* functions */
+    inline bool isFile() const;
+    inline bool isFile(const std::string&) const;
+    void output(std::ostream& os = std::cout) const
+        {
+            if (valid) os << baseDir << "/" << packageDir << std::endl;
+            else os << filename << " not valid!" << std::endl;
+        }
     int unrar() const;
+    bool isValid() { return valid; }
 private:
     std::string filename;
+    std::string packageDir;
+    bool valid = true;
 };
 
+/*************
+ * variables *
+ *************/
 std::string Package::baseDir;
+std::string Package::extension = ".rsn";
 
-Package::Package(std::string fname) {
-    if (fname[fname.size() - 1] == '/') {
-        throw "Error: entered a directory as a filename.";
+/*************
+ * functions *
+ *************/
+Package::Package(std::string fname)
+    : filename(fname)
+{
+    if (!isFile(filename)) {
+        valid = false;
+        return;
     }
 
-    auto slashPos = fname.find_last_of('/');
+    // if not found, string::npos + 1 == 0
+    auto slash = filename.find_last_of('/');
+    // move past the slash
+    ++slash;
 
-    // don't need to handle the empty case because argv won't send empty strings.
-    if (slashPos == std::string::npos) {
-        filename = fname;
-    } else {
-        filename = fname.substr(slashPos + 1, std::string::npos);
+    size_t extensionSize = extension.size();
+    if (filename.substr(filename.size() - extension.size()) != extension) {
+        extensionSize = 0;
     }
+
+    packageDir = filename.substr(slash, filename.size() - slash - extensionSize);
+}
+
+bool Package::isFile(const std::string &filename) const {
+    struct stat info;
+    // stat succeeds && not a directory
+    return !stat(filename.c_str(), &info) && !(info.st_mode & S_IFDIR);
+}
+
+bool Package::isFile() const {
+    return isFile(filename);
 }
 
 int Package::unrar() const {
