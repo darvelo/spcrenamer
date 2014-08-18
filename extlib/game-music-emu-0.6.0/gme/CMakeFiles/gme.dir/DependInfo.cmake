e11a3cc0a54e869d0391d4bbee2e53d471da11db
David Arvelo <david@davidarvelo.com>
2014-08-17 23:21:32 -0400

    Prelim game music metadata support

---
+++ extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Ay_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Ay_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Ay_Cpu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Ay_Cpu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Ay_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Ay_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Blip_Buffer.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Blip_Buffer.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Classic_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Classic_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Data_Reader.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Data_Reader.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Dual_Resampler.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Dual_Resampler.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Effects_Buffer.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Effects_Buffer.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Fir_Resampler.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Fir_Resampler.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Gb_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Gb_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Gb_Cpu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Gb_Cpu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Gb_Oscs.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Gb_Oscs.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Gbs_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Gbs_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Gme_File.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Gme_File.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Gym_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Gym_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Hes_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Hes_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Hes_Cpu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Hes_Cpu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Hes_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Hes_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Kss_Cpu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Kss_Cpu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Kss_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Kss_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Kss_Scc_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Kss_Scc_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/M3u_Playlist.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/M3u_Playlist.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Multi_Buffer.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Multi_Buffer.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Music_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Music_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Nes_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Nes_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Nes_Cpu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Nes_Cpu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Nes_Fme7_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Nes_Fme7_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Nes_Namco_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Nes_Namco_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Nes_Oscs.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Nes_Oscs.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Nes_Vrc6_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Nes_Vrc6_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Nsf_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Nsf_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Nsfe_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Nsfe_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Sap_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Sap_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Sap_Cpu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Sap_Cpu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Sap_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Sap_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Sms_Apu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Sms_Apu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Snes_Spc.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Snes_Spc.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Spc_Cpu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Spc_Cpu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Spc_Dsp.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Spc_Dsp.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Spc_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Spc_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Spc_Filter.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Spc_Filter.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Vgm_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Vgm_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Vgm_Emu_Impl.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Vgm_Emu_Impl.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Ym2413_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Ym2413_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/Ym2612_Emu.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/Ym2612_Emu.cpp.o"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/gme.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/gme.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "BLARGG_BUILD_DLL"
+  "LIBGME_VISIBILITY"
+  )
+
+# Pairs of files generated by the same build rule.
+SET(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/libgme.so" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/libgme.so.0.6.0"
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/libgme.so.0" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/libgme.so.0.6.0"
+  )
+
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "gme"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- src/Package.hpp
+++ src/Package.hpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "../extlib/game-music-emu-0.6.0/gme/gme.h"
+#include "../extlib/game-music-emu-0.6.0/gme/Music_Emu.h"
 
 class Package {
 public:
@@ -19,17 +21,21 @@ public:
     /* functions */
     inline bool isFile() const;
     inline bool isFile(const std::string&) const;
-    void output(std::ostream& os = std::cout) const
-        {
-            if (valid) os << baseDir << "/" << packageDir << std::endl;
-            else os << filename << " not valid!" << std::endl;
+    void output(std::ostream&);
+    void handle_error(const char* str) const {
+        if (str) {
+            printf( "Error: %s\n", str ); getchar();
+            exit( EXIT_FAILURE );
         }
+    }
     int unrar() const;
     bool isValid() { return valid; }
 private:
     std::string filename;
     std::string packageDir;
     bool valid = true;
+	long sample_rate = 44100;
+    Music_Emu* emu;
 };
 
 /*************
@@ -70,6 +76,40 @@ bool Package::isFile(const std::string &filename) const {
 
 bool Package::isFile() const {
     return isFile(filename);
+}
+
+void Package::output(std::ostream& os = std::cout) {
+    // Determine file type
+    gme_type_t file_type;
+    handle_error( gme_identify_file( filename.c_str(), &file_type ) );
+    if ( !file_type )
+        handle_error( "Unsupported music type" );
+
+    gme_open_file(filename.c_str(), &emu, sample_rate);
+
+    gme_info_t* trackInfo;
+    gme_track_info( emu, &trackInfo, 0 );
+
+    std::cout 
+        /* times in milliseconds; -1 if unknown */
+        << trackInfo->length << std::endl
+        << trackInfo->intro_length << std::endl
+        << trackInfo->loop_length << std::endl
+        /* empty string if not available */
+        << trackInfo->system << std::endl
+        << trackInfo->game  << std::endl
+        << trackInfo->song << std::endl
+        << trackInfo->author  << std::endl
+        << trackInfo->copyright  << std::endl
+        << trackInfo->comment   << std::endl
+        << trackInfo->dumper   << std::endl
+        << std::endl;
+
+    gme_free_info( trackInfo );
+    delete emu;
+
+    if (valid) os << baseDir << "/" << packageDir << std::endl;
+    else os << filename << " not valid!" << std::endl;
 }
 
 int Package::unrar() const {
