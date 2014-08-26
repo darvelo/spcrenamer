53b60d7afe0f1bc373ca195fd661daa5bc2263ed
David Arvelo <david@davidarvelo.com>
2014-08-25 23:41:01 -0400

    Add the rest of the game-music-emu-0.6.0 library files just in case

---
+++ extlib/game-music-emu-0.6.0/demo/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/demo/basics.c" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/demo/CMakeFiles/demo.dir/basics.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/demo/Wave_Writer.cpp" "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/demo/CMakeFiles/demo.dir/Wave_Writer.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "LIBGME_VISIBILITY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/darth/website/project-to-rename-spc-titles/extlib/game-music-emu-0.6.0/gme/CMakeFiles/gme.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "gme"
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ extlib/game-music-emu-0.6.0/demo/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/Wave_Writer.cpp.o"
+  "CMakeFiles/demo.dir/basics.c.o"
+  "demo.pdb"
+  "demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C CXX)
+  INCLUDE(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
